7fc738e4d3051501efa1eabbb79b8812e34750d0
Juan Contreras <Juan_Contreras@csu.fullerton.edu>
2026-01-03 00:08:16 -0800

    defined the functions inside wasm_exports.cpp

--- src/cpp/wasm_exports.cpp
+++ src/cpp/wasm_exports.cpp
@@ -1,7 +1,9 @@
 #include "TripManager.h"
+#include "TripLeg.h"
 
+//Expose functions with C linkage so JS/WASM can find them
 extern "C"
 {
-	double add(double a, double b);
-	double compute_time(double distance, double speed);
+	double add(double a, double b) { return ::add(a, b);}
+	double compute_time(double distance, double speed) { return ::compute_time(distance, speed);}
 }
